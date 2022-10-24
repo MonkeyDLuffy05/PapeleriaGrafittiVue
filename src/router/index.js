8e776f71f87b47f8c7ca35c28e066b1317521b3b
MonkeyDLuffy05 <85811871+MonkeyDLuffy05@users.noreply.github.com>
2022-10-23 23:05:36 -0500

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -20,13 +20,13 @@ const routes = [{
     },
 
     {
-        path: '/Vendedor',
-        name: 'Vendedor',
+        path: '/Cliente',
+        name: 'Cliente',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/Vendedor.vue')
+            import ( /* webpackChunkName: "about" */ '../views/Cliente.vue')
     },
 
 ]
---
+++ src/views/Cliente.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="Cliente">
+        <h1>Bienvenido Cliente</h1>
+        <FormularioFactura />
+        <Factura />
+
+    </div>
+
+</template>
+
+
+
+<script>
+import FormularioFactura from '@/components/formularioFactura.vue';
+import Factura from '../components/factura.vue';
+
+
+export default {
+    name: "Vendedor-view",
+    components: { FormularioFactura, Factura }
+}
+</script>
+
+
+
+<style>
+
+</style>
